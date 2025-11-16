afed66eccd972a214ade1b208600393f64142a57
N1femi <niffybabs@gmail.com>
2025-11-16 07:57:44 -0500

    Finished Extension UI, added alerts

--- popup.js
+++ popup.js
@@ -1,23 +1,33 @@
 const softenBtn = document.querySelector("#soften-btn")
 
 async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+  let queryOptions = { active: true, lastFocusedWindow: true }
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions)
+  return tab
 }
 
-softenBtn.addEventListener("click", async () => {
-    console.log("User wants to Soften!")
+let inserted = false
 
+softenBtn.addEventListener("click", async () => {
+  console.log("User wants to Soften!")
+
+  if (inserted === false) {
     const tab = await getCurrentTab()
     const tabId = tab["id"]
-    
-    chrome.scripting.insertCSS({
+
+    chrome.scripting
+      .insertCSS({
         target: { tabId },
         files: ["softener.css"],
-    })
-        .then(() => {
-            
-        })
+      })
+      .then(() => {
+        inserted = true
+      })
+      
+  } else {
+    alert(
+      "Page has already been softened, if not working submit issue on Github."
+    )
+  }
 })
